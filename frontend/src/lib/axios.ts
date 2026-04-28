b68bee5d1c3cfe7289e7b68558293ea3155286e6
Muhammad Khoirul Anam <mkhoirulanam920@gmail.com>
2026-04-28 14:05:00 +0700

    fix: add axios interceptor for auth token

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8000',
@@ -7,6 +8,17 @@ const api = axios.create({
     Accept: 'application/json',
   },
   withCredentials: true,
+});
+
+// Add a request interceptor
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('auth_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
